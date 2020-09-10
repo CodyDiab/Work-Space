214c4634d3febb23195af0bec1de51794b1494b2
Diab <emailcodydiab@gmail.com>
2020-09-10 09:02:33 -0500

    ANgular REST

--- Angular/angular.js
+++ Angular/angular.js
@@ -1,5 +1,22 @@
-var app = angular.module('app', []);
-app.controller('myCtrl', function($scope) {
-  $scope.firstName= "John";
-  $scope.lastName= "Doe";
+// var app = angular.module('app', []);
+// app.controller('myCtrl', function($scope) {
+//   $scope.firstName= "John";
+//   $scope.lastName= "Doe";
+// });
+
+
+
+// app.directive("directive", function() {
+//   return {
+//       restrict: 'CE',
+//     template : "<h1>Made by a directive!</h1>"
+//   };
+// });
+
+angular.module('app', [])
+.controller('Search', function($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
 });
